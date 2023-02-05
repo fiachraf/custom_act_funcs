58ffa67502e316ca793f9eb03f7b4574c02cdca2
Fiachra <fiachraf12@gmail.com>
2023-02-05 22:21:41 +0000

    updated results and test_NN.py to include ResNet like results

--- test_NN.py
+++ test_NN.py
@@ -40,6 +40,8 @@ training_data = datasets.FashionMNIST(
     transform=ToTensor(),
 )
 
+#there are 60,000 images in the training dataset
+
 # Download test data from open datasets.
 test_data = datasets.FashionMNIST(
     root="data",
@@ -116,7 +118,7 @@ def test(dataloader, model, loss_fn):
 
 import csv
 
-with open("test_results.csv", mode="w") as csv_file:
+with open("ResNet_test_results.csv", mode="w") as csv_file:
     csv_file_writer = csv.writer(csv_file)
     csv_file_writer.writerow(["Activation Function", "Epoch", "Training Accuracy", "Test Accuracy", "Training Loss", "Test Loss", "Time(s)"])
 
@@ -128,25 +130,112 @@ with open("test_results.csv", mode="w") as csv_file:
     #test each function 5 times in order to caluclate statistics
         for i in range(1,6):
         # Define model
-            class NeuralNetwork(nn.Module):
-                def __init__(self):
+        #small model
+            # class NeuralNetwork(nn.Module):
+            #     def __init__(self):
+            #         super().__init__()
+            #         self.flatten = nn.Flatten()
+            #         self.linear_relu_stack = nn.Sequential(
+            #             nn.Linear(28*28, 512),
+            #             #func is the activation that is going to be used
+            #             func,
+            #             nn.Linear(512, 512),
+            #             func,
+            #             nn.Linear(512, 10)
+            #         )
+            #
+            #     def forward(self, x):
+            #         x = self.flatten(x)
+            #         logits = self.linear_relu_stack(x)
+            #         return logits
+
+            #larger resnet inspired model to test if my functions scale well to deep models like ReLU does
+            #create the ResidualBlock for use in the ResNet
+            class ResidualBlock(nn.Module):
+                def __init__(self, in_channels, out_channels, stride=1, downsample=None):
                     super().__init__()
-                    self.flatten = nn.Flatten()
-                    self.linear_relu_stack = nn.Sequential(
-                        nn.Linear(28*28, 512),
-                        #func is the activation that is going to be used
-                        func,
-                        nn.Linear(512, 512),
-                        func,
-                        nn.Linear(512, 10)
+
+                    self.conv1 = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
+                        nn.BatchNorm2d(out_channels),
+                        #my chosen activation function
+                        func
                     )
+                    self.conv2 = nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+                        nn.BatchNorm2d(out_channels)
+                    )
+                    self.downsample = downsample
+                    self.act_func = func
+                    self.out_channels = out_channels
 
                 def forward(self, x):
-                    x = self.flatten(x)
-                    logits = self.linear_relu_stack(x)
-                    return logits
+                    residual = x
+                    out = self.conv1(x)
+                    out = self.conv2(out)
+                    if self.downsample:
+                        residual = self.downsample(x)
+                    out += residual
+                    out = self.act_func(out)
+                    return out
 
-            model = NeuralNetwork().to(device)
+            class ResNet(nn.Module):
+                def __init__(self, block, layers, num_classes=10):
+                    super().__init__()
+
+                    self.inplanes  = 64
+                    self.conv1 = nn.Sequential(
+                        #FashionMNIST dataset images are greyscale so 1 input channel
+                        nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=3),
+                        nn.BatchNorm2d(64),
+                        func
+                    )
+                    self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+                    self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
+                    self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
+                    self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
+                    self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
+                    # self.avgpool = nn.AvgPool2d(7, stride=1)
+                    #had to change to 3 cos the FashionMNIST images start off small and would be too small(less than 0 in size) if let at 7
+                    self.avgpool = nn.AvgPool2d(3, stride=1)
+                    self.fc = nn.Linear(512, num_classes)
+
+
+                def _make_layer(self, block, planes, blocks, stride=1):
+                    downsample = None
+                    if stride != 1 or self.inplanes != planes:
+                        downsample = nn.Sequential(
+                        nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                        nn.BatchNorm2d(planes)
+                        )
+                    layers = []
+                    layers.append(block(self.inplanes, planes, stride, downsample))
+                    self.inplanes = planes
+                    for i in range(1, blocks):
+                        layers.append(block(self.inplanes, planes))
+
+                    return nn.Sequential(*layers)
+
+                def forward(self, x):
+                    x = self.conv1(x)
+                    x = self.maxpool(x)
+                    x = self.layer0(x)
+                    x = self.layer1(x)
+                    #need to uncomment these lines and previous lines if I want to have the intended 4 layers
+                    # x = self.layer2(x)
+                    # x = self.layer3(x)
+
+                    x = self.avgpool(x)
+                    x = x.view(x.size(0), -1)
+                    x = self.fc(x)
+
+                    return x
+
+
+
+
+            # model = ResNet(ResidualBlock, [3,1,2,4]).to(device)
+            model = ResNet(ResidualBlock, [3,4,6,3]).to(device)
             print(model)
 
             #Optimizing the model parameters
@@ -174,8 +263,8 @@ with open("test_results.csv", mode="w") as csv_file:
 
             #Saving the model
             #save the model with act func in the name as well iteration
-            torch.save(model.state_dict(), f"{text}_model_{i}.pth")
-            print(f"Saved PyTorch Model State to {text}_model_{i}.pth")
+            torch.save(model.state_dict(), f"{text}_ResNetmodel_{i}.pth")
+            print(f"Saved PyTorch Model State to {text}_ResNetmodel_{i}.pth")
 
             #creating plot
             import matplotlib.pyplot as plt
@@ -184,18 +273,18 @@ with open("test_results.csv", mode="w") as csv_file:
 
             plt.plot(epochs, train_accuracy_list, "bo", label="Train acc")
             # plt.plot(epochs, val_acc, "b", label="Validation acc")
-            plt.title(f"{text}_Train accuracy_{i}")
+            plt.title(f"{text}_ResNetTrain accuracy_{i}")
             plt.legend()
-            plt.savefig(f"{text}_Train accuracy_{i}.png")
+            plt.savefig(f"{text}_ResNetTrain accuracy_{i}.png")
             # plt.figure()
 
             plt.clf()
 
             plt.plot(epochs, test_accuracy_list, "bo", label="Test acc")
             # plt.plot(epochs, val_acc, "b", label="Validation acc")
-            plt.title(f"{text}_Test accuracy_{i}")
+            plt.title(f"{text}_ResNetTest accuracy_{i}")
             plt.legend()
-            plt.savefig(f"{text}_Test accuracy_{i}.png")
+            plt.savefig(f"{text}_ResNetTest accuracy_{i}.png")
 
             plt.clf()
         # plt.figure()
