5e9e037698c7f3da89ac3e253a7f95be14f05194
Fiachra <fiachraf12@gmail.com>
2023-03-02 22:58:51 +0000

    fixed pos_hill_v2 to with full capabilities as intended, had to change the multiplication and addition being done in multiple where statements to be multiple where statements to generate tensors that I could then multiply and add to the input tensor. Added pos_hill_v3, a slight variation on pos_hill_v2, where the 'slope' at turning points should be 0.

--- custom_act_funcs.py
+++ custom_act_funcs.py
@@ -7,11 +7,14 @@ from torchvision.transforms import ToTensor
 #I had initially include an argument for each function to have it operate in place but it didn't work properly for some so I just removed that
 #if input negative, output = input, else output = 0 (i.e. for positives)
 def neg_relu(x):
-    y = torch.clone(x)
-    y[y>0] = 0
-    return y
+    # y = torch.clone(x)
+    #this index operation counts as an inplace operation and so it doesn't propagate gradients correctly without the previous clone step and even then it might not be propagaing gradients properly
+    # y[y>0] = 0
+    #however the torch.where function propagates gradients correctly and also removes the extra memory I'm probably using up from the clone statement
+    x = torch.where(x<0, x, 0)
+    return x
 
-#has to be implemented as class
+#has to be implemented as class in order to be used in a NeuralNetwork
 class NegReLU(nn.Module):
     def __init__(self):
         super().__init__()  #init the base class
@@ -25,10 +28,8 @@ class NegReLU(nn.Module):
 def dslope_relu(x, dslope=1.5):
     #could possibly make dslope another input variable and also potentially a trainable parameter
     #value of dslope should be changed and tested for different values
-    # dslope = 1.5
-    y = torch.clone(x)
-    y[y<0] = 0
-    return dslope * y
+    x = torch.where(x < 0, 0, x*dslope)
+    return x
 class DSlopeReLU(nn.Module):
     def __init__(self, dslope=1.5):
         super().__init__()
@@ -42,11 +43,8 @@ class DSlopeReLU(nn.Module):
 #relu except the y intercept is changed and all positive values are increased/decreased by the value of the y intercept thus keeping the slope at 1
 def diff_y_relu(x, y_int=0.5):
     #could possibly make y_int another input variable and also potentially a trainable parameter
-    # y_int = 0.5
-    y = torch.clone(x)
-    y[y<0] = 0
-    y[y>0] += y_int
-    return y
+    x = torch.where(x<0, 0, x + y_int)
+    return x
 class Diff_Y_ReLU(nn.Module):
     def __init__(self, y_int=0.5):
         super().__init__()
@@ -62,12 +60,11 @@ def pos_hill(x, turn_point=0.75):
     # need to change both the up_slope and down_slope at the same time to maintain the continuity of the line
     up_slope = 1/turn_point
     down_slope = -1/(1 - turn_point)
-    y = torch.clone(x)
-    y[y<0] = 0
-    y[y>1] = 0
+    # set values below 0 or above 1 to 0
+    x = torch.where((x < 0) | (x > 1), 0, x)
     # the down_slope is also equal to the negative of the y intercept for the downward part of the hill
-    y = torch.where(y<0.75, y*up_slope, y*down_slope - down_slope)
-    return y
+    x = torch.where(x<0.75, x*up_slope, x*down_slope - down_slope)
+    return x
 class Pos_Hill(nn.Module):
     def __init__(self, turn_point=0.75):
         super().__init__()
@@ -81,10 +78,8 @@ class Pos_Hill(nn.Module):
 #positive inputs are treated like relu, negative inputs are multiplied by a decimal
 def small_neg(x, neg_slope=0.2):
     #neg_slope could be varied and also be used as a trainable parameter
-    # neg_slope = 0.2
-    y = torch.clone(x)
-    y[y<0] *= neg_slope
-    return y
+    x = torch.where(x > 0, x, x * neg_slope)
+    return x
 class Small_Neg(nn.Module):
     def __init__(self, neg_slope=0.75):
         super().__init__()
@@ -95,49 +90,114 @@ class Small_Neg(nn.Module):
         #passed test for gradient propagation
 
 #pos hill except with more control over wher the hill starts, peaks and ends. Each of these points is specified using tuples(x,y)
-#start and end point have to have the same y value or else the function will not be as expected
-def pos_hill_v2(x, start_p=(0,0), peak_p=(1,1), end_p=(2,0)):
-    y = torch.clone(x)
-    up_slope = (peak_p[1] - start_p[1]) / (peak_p[0] - start_p[0])
-    down_slope = (end_p[1] - peak_p[1]) / (end_p[0] - peak_p[0])
-    c1 = start_p[1] - up_slope * start_p[0]
-    c2 = end_p[1] - down_slope * end_p[0]
-    # y[y<start_p[0]] = start_p[1]
-    # y = torch.where((y<peak_p[0]) & (y>start_p[0]), y*up_slope + c1, y*down_slope + c2)
-    # y = torch.where((y<peak_p[0]) & (y>start_p[0]), y*up_slope + c1, (torch.where((y>peak_p[0]) & (y<end_p[0]), y*down_slope + c2),))
-    # left = torch.where((y<peak_p[0]) & (y>start_p[0]), y* up_slope + c1, y)
-    # right = torch.where((y>peak_p[0]) & (y<end_p[0]), y* down_slope + c2, y)
-    # truth_table = torch.where(torch.eq(left, y))
-    # truth_table = torch.where(torch.eq(left, y))
-    # rejoin = torch.where((a := 1 if truth_table else 0) > 0, left, right)
+#start and end point can have different y values
+def pos_hill_v2(x, start_p=(0.0,0.0), peak_p=(1.0,1.0), end_p=(2.0,0.0)):
 
-    # this one doesn't works
-    left = torch.where((y<=peak_p[0]) & (y>start_p[0]), y* up_slope + c1, start_p[1])
-    right = torch.where((y>peak_p[0]) & (y<end_p[0]), y* down_slope + c2, end_p[1])
-    new = torch.where(left>right, left, right)
+    #changing the tuples to be tensors with the same dtype as the input tensor x, as torch.where requires all arguments to have the same dtype
+    t_start_p = x.new([start_p[0], start_p[1]])
+    t_peak_p = x.new([peak_p[0], peak_p[1]])
+    t_end_p = x.new([end_p[0], end_p[1]])
 
-    #get the "left" slope of the hill to the correct values
-    # left = torch.where((y<=start_p[0]) | (y>peak_p[0]), 0, y)
-    # #maybe split previous line into two lines to separate the or condition so that one can set one side of the peak to be = start_p[1] and the other side of the peak can end_p[1], would allow for the two points to have different y values
-    # left = torch.where((left<peak_p[0]) & (left>start_p[0]), left* up_slope + c1, left)
-    # #get the "right" slope of the hill to the correct values
-    # right = torch.where((y>end_p[0]) | (y<peak_p[0] ), 0, y)
-    # right = torch.where((right>peak_p[0]) & (right<=end_p[0]), right* down_slope + c2, right)
-    # y = torch.where(y<=start_p[0], 0, y)
-    # y = torch.where(y>peak_p[0], 0, y)
-    # previous two lines set vals outside of left slope to 0
-    #does comparison betwenn left slope and "original"(where original values are only exisiting for an in the left slope area), for any values that don't match it inserts the "left" value, otherwise it inserts the "right" value
-    # new = torch.where(left!=y, left, right)
-    # y[y>end_p[1]] = end_p[1]
-    return new
+    #determining the slope and y intercepts for the lines from the given points
+    up_slope = (t_peak_p[1] - t_start_p[1]) / (t_peak_p[0] - t_start_p[0])
+    down_slope = (t_end_p[1] - t_peak_p[1]) / (t_end_p[0] - t_peak_p[0])
+    c1 = t_start_p[1] - up_slope * t_start_p[0]
+    c2 = t_end_p[1] - down_slope * t_end_p[0]
+
+    #creating other tensors, one for multiplication and one for addition.
+    #By doing it with tensors it makes it much easier to do Backpropagation
+    #the multiplication is the multiplication by the slope and the addition is the adding the y intercept
+    #could change to make the turning point multiplied by 0
+
+    mul_tens = torch.empty_like(x)
+    add_tens = torch.empty_like(x)
+
+    #creating a tensor with the same dtype and device as input x, to use to get the 0 slope, regardless of the input points used
+    t_zero = x.new([0.0])
+
+    #for inputs less than the start point (by default 0)
+    mul_tens = torch.where(x<t_start_p[0], t_zero[0], mul_tens)
+    #for inputs between the start and mid points
+    mul_tens = torch.where((x>=t_start_p[0]) & (x<t_peak_p[0]), up_slope, mul_tens)
+    #for inputs between the mid and end points
+    mul_tens = torch.where((x>=t_peak_p[0]) & (x<t_end_p[0]), down_slope, mul_tens)
+    #for inputs greater than the end point (by default 2.0)
+    mul_tens = torch.where(x>=t_end_p[0], t_zero[0], mul_tens)
+
+    #doing the same thing for the addition tensor
+    add_tens = torch.where(x<t_start_p[0], t_start_p[1], add_tens)
+    add_tens = torch.where((x>=t_start_p[0]) & (x<t_peak_p[0]), c1, add_tens)
+    add_tens = torch.where((x>=t_peak_p[0]) & (x<t_end_p[0]), c2, add_tens)
+    add_tens = torch.where(x>=t_end_p[0], t_end_p[1], add_tens)
+
+    x = torch.mul(x, mul_tens)
+    x = torch.add(x, add_tens)
+    return x
 class Pos_Hill_V2(nn.Module):
-    def __init__(self, start_p=(0,0), peak_p=(1,1), end_p=(2,0)):
+    def __init__(self, start_p=(0.0,0.0), peak_p=(1.0,1.0), end_p=(2.0,0.0)):
         super().__init__()
         self.start_p = start_p
         self.peak_p = peak_p
         self.end_p = end_p
     def forward(self, input):
         return pos_hill_v2(input, self.start_p, self.peak_p, self.end_p)
+
+#pos hill v2 except the slope at turning points including the peak are set to 0.
+def pos_hill_v3(x, start_p=(0.0,0.0), peak_p=(1.0,1.0), end_p=(2.0,0.0)):
+
+    #changing the tuples to be tensors with the same dtype as the input tensor x, as torch.where requires all arguments to have the same dtype
+    t_start_p = x.new([start_p[0], start_p[1]])
+    t_peak_p = x.new([peak_p[0], peak_p[1]])
+    t_end_p = x.new([end_p[0], end_p[1]])
+
+    #determining the slope and y intercepts for the lines from the given points
+    up_slope = (t_peak_p[1] - t_start_p[1]) / (t_peak_p[0] - t_start_p[0])
+    down_slope = (t_end_p[1] - t_peak_p[1]) / (t_end_p[0] - t_peak_p[0])
+    c1 = t_start_p[1] - up_slope * t_start_p[0]
+    c2 = t_end_p[1] - down_slope * t_end_p[0]
+
+    #creating other tensors, one for multiplication and one for addition.
+    #By doing it with tensors it makes it much easier to do Backpropagation
+    #the multiplication is the multiplication by the slope and the addition is the adding the y intercept
+    #could change to make the turning point multiplied by 0
+
+    mul_tens = torch.empty_like(x)
+    add_tens = torch.empty_like(x)
+
+    #creating a tensor with the same dtype and device as input x, to use to get the 0 slope, regardless of the input points used
+    t_zero = x.new([0.0])
+
+    #for inputs less than the start point (by default 0)
+    mul_tens = torch.where(x<=t_start_p[0], t_zero[0], mul_tens)
+    #for inputs between the start and mid points
+    mul_tens = torch.where((x>t_start_p[0]) & (x<t_peak_p[0]), up_slope, mul_tens)
+    #getting the slope at the peak turning point to be 0
+    mul_tens = torch.where(x==t_peak_p[0], t_zero, mul_tens)
+    #for inputs between the mid and end points
+    mul_tens = torch.where((x>t_peak_p[0]) & (x<t_end_p[0]), down_slope, mul_tens)
+    #for inputs greater than the end point (by default 2.0)
+    mul_tens = torch.where(x>=t_end_p[0], t_zero[0], mul_tens)
+
+    #doing the same thing for the addition tensor
+    add_tens = torch.where(x<=t_start_p[0], t_start_p[1], add_tens)
+    add_tens = torch.where((x>t_start_p[0]) & (x<t_peak_p[0]), c1, add_tens)
+    add_tens = torch.where(x==t_peak_p[0], t_peak_p[1], add_tens)
+    add_tens = torch.where((x>t_peak_p[0]) & (x<t_end_p[0]), c2, add_tens)
+    add_tens = torch.where(x>=t_end_p[0], t_end_p[1], add_tens)
+
+    x = torch.mul(x, mul_tens)
+    x = torch.add(x, add_tens)
+    return x
+class Pos_Hill_V3(nn.Module):
+    def __init__(self, start_p=(0.0,0.0), peak_p=(1.0,1.0), end_p=(2.0,0.0)):
+        super().__init__()
+        self.start_p = start_p
+        self.peak_p = peak_p
+        self.end_p = end_p
+    def forward(self, input):
+        return pos_hill_v3(input, self.start_p, self.peak_p, self.end_p)
+
+
 
 def double_hill(x, start_p=(-2,0), peak_1=(-1,1), mid_p=(0,0), peak_2=(1,1), end_p=(2,0)):
         y = torch.clone(x)
@@ -213,15 +273,41 @@ class Val_Hill(nn.Module):
     def forward(self, input):
         return val_hill(input, self.start_p, self.val_p, self.peak_p, self.end_p)
 
-act_func_dict = {"neg_relu" : neg_relu, "dslope_relu" : dslope_relu, "diff_y_relu" : diff_y_relu, "pos_hill" : pos_hill, "small_neg" : small_neg, "pos_hill_v2" : pos_hill_v2, "double_hill": double_hill, "val_hill" : val_hill}
+act_func_dict = {"neg_relu" : neg_relu, "dslope_relu" : dslope_relu, "diff_y_relu" : diff_y_relu, "pos_hill" : pos_hill, "small_neg" : small_neg, "pos_hill_v2" : pos_hill_v2, "pos_hill_v3" : pos_hill_v3, "double_hill": double_hill, "val_hill" : val_hill}
 act_class_dict = {"NegReLU": NegReLU(), "DSlopeReLU": DSlopeReLU(), "Diff_Y_ReLU": Diff_Y_ReLU(), "Pos_Hill" : Pos_Hill(), "Small_Neg": Small_Neg()}
 
 
 if __name__ == '__main__':
 
     #test gradient calculation
-    x=torch.tensor([-1.0, 0.5], requires_grad=True)
-    y = torch.tensor([0.75, 0.1], requires_grad=True)
+    x=torch.tensor([[-1.0, -1.1, -1.2], [0.5, 0.6, 0.7]], requires_grad=True)
+    y = torch.tensor([[0.75, 0.76, 0.77], [0.1, 0.2, 0.3]], requires_grad=True)
+    y1 = torch.tensor([[0.85, 0.86, 0.87], [0.91, 0.92, 0.93]], requires_grad=True)
+    y7 = torch.tensor([[0.85, 0.86, 0.87], [0.91, 0.92, 0.93]])
+    print(f"y7: {y7}")
+    y7_v = y7.view(-1)
+    print(f"y7_v: {y7_v}")
+    y7_v[0] = 0
+    print(f"y7_v: {y7_v}")
+    # x=torch.tensor([-1.0, -1.1, -1.2], requires_grad=True)
+    # y = torch.tensor([0.75, 0.76, 0.77], requires_grad=True)
+    # y1 = torch.tensor([0.85, 0.86, 0.87], requires_grad=True)
+    x_v = x.view(-1)
+    y_v = y.view(-1)
+    y1_v = y1.view(-1)
+    new_1 = torch.empty_like(x_v)
+    for index, val in enumerate(x_v):
+        print(index, val)
+        new_1[index] = val * index
+    print(f"new_1: {new_1}")
+    # print(f"new_1[0]: {new_1[0]}, new_1.backward():{new_1.backward()}, new_1.grad_fn: {new_1.grad_fn}, new_1.grad_fn.next_functions: {new_1.grad_fn.next_functions}, ")
+    print(f"new_1[0]: {new_1[0]}, new_1.grad_fn: {new_1.grad_fn}, new_1.grad_fn.next_functions: {new_1.grad_fn.next_functions}, ")
+
+    print(f"x: {x}")
+    print(f"x_v: {x_v}")
+    # x_v[-1] = x_v[-1] + 7
+    print(f"x: {x}")
+    print(f"x_v: {x_v}")
     # x=torch.tensor([-1.0], requires_grad=True)
     # y = torch.tensor([0.75], requires_grad=True)
     # z = x * y
@@ -231,13 +317,34 @@ if __name__ == '__main__':
     # a = diff_y_relu(z)
     # a = pos_hill(z)
     # a = small_neg(z, False, 0.2)
-    # a = neg_relu(z)
     a = pos_hill_v2(z)
+    # a = pos_hill_v2(z)
+    stacked = torch.stack((x,y,y1), -1)
+    print(f"stacked.size(): {stacked.size()}")
+    print(f"stacked: {stacked}")
+    for i, p in enumerate(stacked):
+        print(p)
+    stacked_v = torch.stack((x_v,y_v,y1_v), -1)
+    print(f"stacked_v.size(): {stacked_v.size()}")
+    print(f"stacked_v: {stacked_v}")
+    for i, p in enumerate(stacked_v):
+        print(p)
 
     # grad_1 = torch.autograd.grad(a, (x, y, z, a), create_graph=True)
     # print(f"x: {x.item()}, y: {y.item()}, z: {z.item()}, a: {a.item()}")
     print(f"x: {x}, y: {y}, z: {z}, a: {a}")
+    # print(f"a[0]: {a[0]}, a.backward():{a.backward()}, a.grad_fn: {a.grad_fn}, a.grad_fn.next_functions: {a.grad_fn.next_functions}, ")
     # print(f"grad_1: {grad_1}")
+    from torch.autograd import gradcheck
+
+    # x2 = torch.rand(256, dtype=torch.double, requires_grad=True)
+    # x3 = torch.rand(256, dtype=torch.double, requires_grad=True)
+    # x4 = torch.rand(256, dtype=torch.double, requires_grad=True)
+    # x5 = torch.rand(256, dtype=torch.double, requires_grad=True)
+
+    #gradcheck first arg is the function to check, 2nd arg is a tuple of inputs to use for testing, typically a torch.rand() tensor, if the test function only takes one input argument then the tuple should be in the form (torch.rand(),)
+    # grad_test = gradcheck(pos_hill_v2, (x2,x3,x4,x5))
+    # print(f"grad_test: {grad_test}")
 
     make_plots = input("Do you want to make plots of the activation functions? Y/N: ")
     if make_plots == "Y":
